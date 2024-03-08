9897d51171544a3dbd51be87e414e0ceaa7e449e
na2vjot <145135646+na2vjot@users.noreply.github.com>
2024-03-08 23:34:24 +0530

    staff_management.py

--- staffwdb.py
+++ staffwdb.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector as sql
 
-conn=sql.connect(host='Localhost',user='root',passwd='password',database='restaurant')
+conn=sql.connect(host='Localhost',user='root',passwd='n@b123BU',database='restaurant')
 if conn.is_connected():
     print('successfully connected')
 
 #creating staff table if it doesnt exist
 def create_staff():
     c1=conn.cursor()
-    c1.execute('create table if not exists staff_management(staff_id INT PRIMARY KEY AUTO_INCREMENT,name VARCHAR(50),Role VARCHAR(50),age INT,salary INT)')
+    create="""CREATE TABLE IF NOT EXISTS staff_management(staff_id INT PRIMARY KEY AUTO_INCREMENT,name VARCHAR(50),Role VARCHAR(50),age INT,contact_number VARCHAR(50),salary INT)"""
+    c1.execute(create)
+
+create_staff()
 
 #displaying existing staff details
 def display_staff():
@@ -24,7 +27,7 @@ display_staff()
 #managing/entering new staff details
 def manage_staff():
     c1=conn.cursor()
-    squery="""INSERT INTO staff_management(name,Role,age,salary) VALUES (%s, %s, %s,%s)"""
+    squery="""INSERT INTO staff_management(name,Role,age,salary,contact_number) VALUES (%s, %s, %s,%s,%s)"""
     L1=[]
     name=input('enter the name of the employee:')
     L1.append(name)
@@ -34,10 +37,12 @@ def manage_staff():
     L1.append(age)
     salary=int(input('salary of the employee:'))
     L1.append(salary)
+    contact_number=input('enter the contact number:')
+    L1.append(contact_number)
     c1.execute(squery,L1)
     print("Employee added successfully.")
 
 
-create_staff()
+
 manage_staff()
 conn.commit() #commiting the changes made in db
